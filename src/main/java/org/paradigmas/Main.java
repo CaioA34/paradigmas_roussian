72db7248f4923fe2b9c2e1e96575246489987de3
Caio Almeida <caio.almeida@protestoonline.com.br>
2026-05-29 18:38:29 -0300

    Segunda Parte Código Funcional - Completo

--- src/main/java/org/paradigmas/Main.java
+++ src/main/java/org/paradigmas/Main.java
@@ -37,6 +37,7 @@ Deve-se criar métodos separados para cada operação.
 */
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import Classes.Venda;
 
@@ -62,8 +63,86 @@ public class Main {
         for(Venda venda : result){
             System.out.println(venda);
         }
+        System.out.println("\nVenda com categoria roupas Imperativo: ");
+        List<Venda> byCategory = filtrarRoupasImperativo(vendas);
+        for(Venda venda : byCategory){
+            System.out.println(venda);
+        }
+        System.out.println("\nFaturamento total Imperativo: ");
+        System.out.println(calcularFaturamentoImperativo(vendas));
 
+        System.out.println("\nVenda maior que 200 Imperativo: ");
+        List<Venda> maiorQue = vendasAcimaDe200Imperativo(vendas);
+        for(Venda venda : maiorQue) {
+            System.out.println(venda);
+        }
+
+        System.out.println("\nNome produtos Imperativo: ");
+        List<String> produtos = nomesProdutosImperativo(vendas);
+        for(String nome : produtos){
+            System.out.println(nome);
+        }
+
+        System.out.println("\nfiltrarRoupasFuncional: ");
+        System.out.println(filtrarRoupasFuncional(vendas));
+
+        System.out.println("\ncalcularFaturamentoFuncional");
+        System.out.println(calcularFaturamentoFuncional(vendas));
+
+        System.out.println("vendasAcimaDe200Funcional");
+        System.out.println(vendasAcimaDe200Funcional(vendas));
+
+        System.out.println("nomesProdutosFuncional");
+        System.out.println(nomesProdutosFuncional(vendas));
+
+        System.out.println("totalPorVendedorFuncional: ");
+        System.out.println(totalPorVendedorFuncional(vendas));
+        System.out.println("\nmaiorVendaFuncional: ");
+        System.out.println(maiorVendaFuncional(vendas));
+        System.out.println("\nordenarPorTotalFuncional: ");
+        List<Venda> result2 = ordenarPorTotalFuncional(vendas);
+        for(Venda venda : result2){
+            System.out.println(venda);
+        }
     }
+
+    //solução imperativa
+    public static List<Venda> filtrarRoupasImperativo(List<Venda> vendas) {
+        List<Venda> result = new ArrayList<>();
+        for(Venda venda : vendas){
+            if(venda.getCategoria().equals("Roupas")){
+                result.add(venda);
+            }
+        }
+        return result;
+    }
+
+    public static double calcularFaturamentoImperativo(List<Venda> vendas) {
+        double acum = 0;
+        for(Venda venda : vendas){
+            acum += venda.getTotal();
+        }
+        return acum;
+    }
+
+    public static List<Venda> vendasAcimaDe200Imperativo(List<Venda> vendas) {
+        List<Venda> result = new ArrayList<>();
+        for(Venda venda : vendas) {
+            if (venda.getTotal() > 200) {
+                result.add(venda);
+            }
+        }
+        return result;
+    }
+
+    public static List<String> nomesProdutosImperativo(List<Venda> vendas) {
+        List<String> result = new ArrayList<>();
+        for(Venda venda : vendas) {
+            result.add(venda.getProduto());
+        }
+        return result;
+    }
+
     public static Map<String, Double> totalPorVendedorImperativo(List<Venda> vendas) {
         Map<String, Double> map= new HashMap<>();
         for(Venda venda : vendas){
@@ -76,8 +155,8 @@ public class Main {
             }
         }
         return map;
-        // implementar
     }
+
     public static Venda maiorVendaImperativo(List<Venda> vendas) {
         Venda maior = vendas.get(0);
         for(Venda venda : vendas){
@@ -86,11 +165,40 @@ public class Main {
             }
         }
         return maior;
-// implementar
     }
+
     public static List<Venda> ordenarPorTotalImperativo(List<Venda> vendas) {
         List<Venda> result = new ArrayList<>(vendas);
         result.sort(Comparator.comparing(Venda::getTotal).reversed());
         return result;
+    }
+
+    //solucao funcional
+    public static List<Venda> filtrarRoupasFuncional(List<Venda> vendas) {
+        return vendas.stream().filter(venda -> venda.getCategoria().equals("Roupas")).toList();
+    }
+
+    public static double calcularFaturamentoFuncional(List<Venda> vendas) {
+        return vendas.stream().mapToDouble(Venda::getTotal).sum();
+    }
+
+    public static List<Venda> vendasAcimaDe200Funcional(List<Venda> vendas) {
+        return vendas.stream().filter(venda -> venda.getTotal() > 200).toList();
+    }
+
+    public static List<String> nomesProdutosFuncional(List<Venda> vendas) {
+        return vendas.stream().map(Venda::getProduto).toList();
+    }
+    public static Map<String, Double> totalPorVendedorFuncional(List<Venda> vendas) {
+        return vendas.stream().collect(Collectors.groupingBy(
+                Venda::getVendedor,
+                Collectors.summingDouble(Venda::getTotal)
+        ));
+    }
+    public static Venda maiorVendaFuncional(List<Venda> vendas){
+        return vendas.stream().max(Comparator.comparing(Venda::getTotal)).orElse(null);
+    }
+    public static List<Venda> ordenarPorTotalFuncional(List<Venda> vendas){
+        return vendas.stream().sorted(Comparator.comparing(Venda::getTotal)).toList();
     }
 }
